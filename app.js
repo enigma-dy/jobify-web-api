8cd69d7c823d197532b03f02ddb643dd402e5a0c
oluwagbenga adedire <135831211+enigma-dy@users.noreply.github.com>
2025-03-25 10:42:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const corsOptions = {
     const allowedOrigins = [
       "https://jobify-liart-two.vercel.app",
       "http://localhost:5173",
-      "https://oluwagbengaapidocs.netlify.app/",
-      "https://webservice-porfolio.vercel.app/",
+      "https://oluwagbengaapidocs.netlify.app",
+      "https://webservice-porfolio.vercel.app",
       "https://scintillating-kitten-95c111.netlify.app"
     ];
     if (allowedOrigins.includes(origin) || !origin) {
